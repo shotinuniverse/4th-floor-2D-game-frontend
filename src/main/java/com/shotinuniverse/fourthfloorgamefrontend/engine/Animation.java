4b9b673f438b9b9ff254e705f862bdf831027b90
vladislav permichev <vvpermichev@1cbit.ru>
2024-03-17 13:14:33 +0300

    #14 - переписано хранение текущих анимаций под динамический массив массива чисел. Виды анимаций (в прыжке, в движении) переделаны под состояния

--- src/main/java/com/shotinuniverse/fourthfloorgamefrontend/engine/Animation.java
+++ src/main/java/com/shotinuniverse/fourthfloorgamefrontend/engine/Animation.java
@@ -6,74 +6,122 @@ import javafx.scene.shape.Rectangle;
 
 import java.util.*;
 
+/**
+ * Класс для анимирования объектов
+ */
 public class Animation {
 
+    /**
+     * Массив сущностей объекта БД, представляющие таблицу animations
+     */
     public ArrayList<AnimationEntity> animations = new ArrayList<>();
-    public Map<Integer, Map<Integer, Integer>> mapBeginFrameAnimations = new HashMap<>();
-    public int counterFramesAnimationRest;
+    public ArrayList<Integer[]> beginFramesAnimations = new ArrayList<>();
+    /**
+     * Хранит текущий номер анимации
+     */
+    public int counterFramesAnimation;
+    public int lastState;
 
-    public void animateRest(List<Rectangle> hitBoxes) {
+    /**
+     * Начинает анимацию по определенному состоянию
+     * @param hitBoxes <code>List<Rectangle></code> хит-боксы объекта
+     * @param state <code>int</code> номер текущего состояния. Принимает следующие значения:
+     *              1) 1 - состояние покоя;
+     *              2) 2 - движение без прыжка;
+     *              3) 3 - прыжок без движения;
+     *              4) 4 - прыжок в движении
+    */
+    public void animate(List<Rectangle> hitBoxes, int state) {
 
         int currentFrame = Game.getCurrentFrame();
 
-        int numberOfAnimationsRest = mapBeginFrameAnimations.size() == 0 ? 0 :
-                mapBeginFrameAnimations.get(mapBeginFrameAnimations.keySet().toArray()[0]).size();
-        if (counterFramesAnimationRest == numberOfAnimationsRest) {
-            counterFramesAnimationRest = 0;
-            mapBeginFrameAnimations.clear();
+        int numberOfAnimations = beginFramesAnimations.size() == 0 ? 0 : beginFramesAnimations.get(0).length;
+
+        if (counterFramesAnimation == numberOfAnimations) {
+            counterFramesAnimation = 0;
+            beginFramesAnimations.clear();
         }
 
-        if (counterFramesAnimationRest == 0) {
+        if (counterFramesAnimation == 0) {
             for (AnimationEntity animation : animations) {
-                if (animation.getInMove() == 0) {
+                int stateFromEntity = animation.getState();
+                if (state == stateFromEntity) {
                     int index = animation.getOwnerId();
                     int frame = currentFrame + (animation.getFrameNumber() - 1);
                     frame = frame > Game.framesPerSecond ? frame - Game.framesPerSecond : frame;
-                    if (mapBeginFrameAnimations.containsKey(index)){
-                        mapBeginFrameAnimations.get(index).put(frame, animation.getId());
-                    } else {
-                        Map<Integer, Integer> map = new HashMap<>();
-                        map.put(frame, animation.getId());
-                        mapBeginFrameAnimations.put(index, map);
-                    }
+                    Integer[] values = new Integer[4];
+                    values[0] = index;
+                    values[1] = frame;
+                    values[2] = animation.getId();
+                    values[3] = stateFromEntity;
+
+                    beginFramesAnimations.add(values);
                 }
             }
         }
 
-        boolean madeAnimate = false;
-        for (Integer objectId: mapBeginFrameAnimations.keySet()) {
-            Rectangle rectangle = hitBoxes.get(objectId - 1);
-            Map<Integer, Integer> frameAnimation = mapBeginFrameAnimations.get(objectId);
-            for (Integer frameNumber: frameAnimation.keySet()) {
-                if (frameNumber != currentFrame)
-                    continue;
-                int animationId = frameAnimation.get(frameNumber);
-                for (AnimationEntity animation : animations) {
-                    if (animation.getId() == animationId) {
-                        double xPos = 0;
-                        double yPos = 0;
-                        if (animation.getAction().equals("right shift")) {
-                            xPos = rectangle.getX() + animation.getValue();
-                            yPos = rectangle.getY();
-                        } else if (animation.getAction().equals("left shift")) {
-                            xPos = rectangle.getX() - animation.getValue();
-                            yPos = rectangle.getY();
-                        }
-
-                        rectangle.setX(xPos);
-                        rectangle.setY(yPos);
-                        madeAnimate = true;
-                    }
-                }
-            }
-        }
+        boolean madeAnimate = animateHitBoxes(currentFrame, hitBoxes);
 
         if (madeAnimate)
-            counterFramesAnimationRest += 1;
+            counterFramesAnimation += 1;
     }
 
+    /**
+     * Выполняет поиск анимаций для хит-боксов для текущего кадра и вызывает отрисовку в случае нахождения соответствия
+     * @param currentFrame <code>int</code> номер текущего кадра
+     * @param hitBoxes <code>List<Rectangle></code> хит-боксы объекта в виде массива прямоугольников
+     * @return <code>boolean</code> анимация найдена и отрисовка выполнена
+     */
+    private boolean animateHitBoxes(int currentFrame, List<Rectangle> hitBoxes) {
+        boolean madeAnimate = false;
+
+        for (Integer[] animationInfo: beginFramesAnimations) {
+            if (animationInfo[1] != currentFrame)
+                continue;
+            Rectangle rectangle = hitBoxes.get(animationInfo[0] - 1);
+            for (AnimationEntity animation : animations) {
+                if (animation.getId() == animationInfo[2]) {
+                    animateBasedOnAction(animation, rectangle);
+                    madeAnimate = true;
+                }
+            }
+        }
+
+        return madeAnimate;
+    }
+
+    /**
+     * Выполняет отрисовку анимации по заданному действию (в БД), посредством движения хит-боксов
+     * @param animation <code>AnimationEntity</code> сущность объекта БД, представляющая строку таблицы animations
+     * @param rectangle <code>Rectangle</code> хит-бокс объекта
+     */
+    private void animateBasedOnAction(AnimationEntity animation, Rectangle rectangle) {
+        String actionName = animation.getAction();
+
+        if (actionName.equals("right shift")) {
+            double xPos = rectangle.getX() + animation.getValue();
+            double yPos = rectangle.getY();
+
+            rectangle.setX(xPos);
+            rectangle.setY(yPos);
+        } else if (actionName.equals("left shift")) {
+            double xPos = rectangle.getX() - animation.getValue();
+            double yPos = rectangle.getY();
+
+            rectangle.setX(xPos);
+            rectangle.setY(yPos);
+        } else if (actionName.equals("rotate clockwise")) {
+            rectangle.setRotate(-animation.getValue());
+        } else if (actionName.equals("rotate counter-clockwise")) {
+            rectangle.setRotate(animation.getValue());
+        }
+    }
+
+    /**
+     * Откатывает инициализированные анимации
+     */
     public void rollbackAnimate() {
-        counterFramesAnimationRest = 0;
-        mapBeginFrameAnimations.clear();
+        counterFramesAnimation = 0;
+        beginFramesAnimations.clear();
     }
 }
